3c60736deaef1fffd40d69c7e1dc4b6ea5047e44
Kenneth Rivadeneira <40522511+kennethriva@users.noreply.github.com>
2018-09-18 11:08:18 +0200

    Update Full Grid_XGB.py

--- Weighted Classification/Full Grid_XGB.py
+++ Weighted Classification/Full Grid_XGB.py
@@ -2,58 +2,105 @@
 """
 Created on Wed Jun 13 14:20:07 2018
 
-@author: Neither do I
+@author: Kenneth Rivadeneira
 """
-
-import os
 import pandas as pd
 from xgboost.sklearn import XGBClassifier
 from sklearn.model_selection import StratifiedKFold, cross_val_score,GridSearchCV,train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, matthews_corrcoef
+from sklearn import metrics
 
-###LOAD FULL DATASET#####
-os.chdir(r"C:\Users\Neither do I\Downloads") # change for original directory
-data = pd.read_csv('results_normalized_filtred.csv')
-X = data.iloc[:,:-1]
-y = data.iloc[:,-1]
-#### SPLIT DATA INTO TRAIN AND TEST SUBSETS #####
-#20% FOR TEST AND 80% FOR TRAIN
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state =256)
+# avoid deprecation warnings
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+###LOAD BOTH TRAIN AND TEST DATASETS#####
+
+path = '' # use path
+train_df = pd.read_csv(path + "/train.csv")
+test_df = pd.read_csv(path + "/test.csv")
+
+# We will use train dataset for hyperparameter tunning and validate them
+# with a split
+
+
+# Drop label from X_train and X_test subsets
+
+X_train = train_df.drop(['Lij'], axis=1)
+y_train = train_df['Lij']
+X_test = test_df.drop(['Lij'], axis=1)
+y_test = test_df['Lij']
 
 # In[GridseacrchCV] 
 
-optimized_XGB = XGBClassifier()
+# To write output
+import sys
+old_stdout = sys.stdout
 
+result_file = open("results.log","w")
+
+sys.stdout = result_file
+
+
+# Tunning parameters and scoring using 'roc_au'
+n = 4 # number of available cores
+
+optimized_XGB = XGBClassifier()
+# change number of threads number of cores available
 parameters = {'nthread':[4], #when use hyperthread, xgboost may become slower
               'objective':['binary:logistic'],
-              'gamma': [0.05,0.1,0.3,0.5,0.7,0.9,1.0],
-              'learning_rate': [0.01,0.015,0.025,0.05,0.1], #so called `eta` value
-              'max_depth': [3,5,6,7,9,12,15,17,25],
-              'min_child_weight': [1,3,5,7],
-              'silent': [0],
-              'subsample': [0.6,0.7,0.8,0.9,1.0],
-              'colsample_bytree': [0.6,0.7,0.8,0.9,1.0],
-              'n_estimators': [50,100], #number of trees
-              'scale_pos_weight': [2.7], # sum(negative cases) / sum(positive cases)
-              'seed': [1337]}
-clf = GridSearchCV(optimized_XGB, parameters, n_jobs=-1, 
-                   cv=StratifiedKFold(n_splits=3, shuffle=True, 
-                                      random_state=1337), 
-                   verbose=2, refit=True,scoring='roc_auc')
-clf.fit(X_train, y_train)
-best_parameters, score, _ = max(clf.grid_scores_, key=lambda x: x[1])
-print('Raw AUC score:', score)
-for param_name in sorted(best_parameters.keys()):
-    print("%s: %r" % (param_name, best_parameters[param_name]))
-    
+              'gamma': [0.1],
+              'learning_rate': [0.05], #so called `eta` value
+              'max_depth': [6],
+              'min_child_weight': [3],
+              'silent': [1],
+              'subsample': [0.6],
+              'colsample_bytree': [0.6],
+              'n_estimators': [50], #number of trees
+              'scale_pos_weight': [2.7], # sum(negative cases) / sum(positive cases) = 2.7 if data is imbalanced
+              'seed': [42]}
 
-# In[Test]
-y_pred = clf.predict(X_test)
-target_names = ['class 0', 'class 1']
-print(classification_report(y_test, y_pred, target_names=target_names))
-print('Matthew coefficient')
+clf = GridSearchCV(optimized_XGB, parameters, n_jobs=n, 
+                   cv=StratifiedKFold(n_splits=5, shuffle=True, 
+                                      random_state=42), 
+                   verbose=1, refit=True,scoring='roc_auc')
+clf.fit(X_train, y_train)
+
+print("Best parameters set found on training set:")
 print()
-print(matthews_corrcoef(y_test, clf.predict(X_test)))
-print('Matriz de confusión')
-conf_mat = confusion_matrix(y_test,clf.predict(X_test))
-print(conf_mat)
+print(clf.best_params_)
+print()
+print("Detailed classification report:")
+print()
+print("The model is trained on the full training set.")
+print("The scores are computed on the full test set.")
+print()
+y_pred = clf.predict(X_test)
+print(classification_report(y_test, y_pred))
+print()
+# probability of being classified as class 1
+y_prob = clf.predict_proba(X_test)[:,1]
+auroc = metrics.roc_auc_score(y_test, y_prob)
+print('AUROC')
+print(auroc)
+print()
+print('F1_score')
+f1_score = metrics.f1_score(y_test, y_pred)
+print(f1_score)
+print()
+print('Accuracy')
+acc = metrics.accuracy_score(y_test, y_pred)
+print(acc)
+print()
+print('Confusion Matrix')
+mat = confusion_matrix(y_test, y_pred)
+print(mat)
+print()
+print('MC')
+MC = matthews_corrcoef(y_test, y_pred)
+print(MC)
+
+sys.stdout = old_stdout
+
+result_file.close()
